d86ea0cad4a116ef7b8eb3479bfb9b4191a0883e
Andrew Zang <andrewz020529@gmail.com>
2023-02-28 21:23:23 -0500

    createRequest method added

--- hardhat-contract-development/contracts/Campaign.sol
+++ hardhat-contract-development/contracts/Campaign.sol
@@ -5,9 +5,24 @@ pragma solidity ^0.8.9;
 // import "hardhat/console.sol";
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        // address recipient; // arguable
+        bool complete;
+    }
+
+    // fields
     address public manager;
     uint256 public minimumContribution;
     mapping(address => uint256) investorsToFunding;
+    Request[] public requests;
+
+    // modifiers
+    modifier restricted() {
+        require(msg.sender == manager, "sender is not campaign manager");
+        _;
+    }
 
     constructor(uint256 minimum) {
         manager = msg.sender;
@@ -18,5 +33,18 @@ contract Campaign {
         require(msg.value > minimumContribution, "not enough fund");
 
         investorsToFunding[msg.sender] += msg.value;
+    }
+
+    function createRequest(
+        string memory description,
+        uint value
+    ) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            complete: false
+        });
+
+        requests.push(newRequest);
     }
 }
